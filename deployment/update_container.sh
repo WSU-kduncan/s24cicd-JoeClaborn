28c0cfae32ef61d091263aa3009e5f6c54bba080
Joseph Claborn <90930349+JoeClaborn@users.noreply.github.com>
2024-04-19 15:46:07 -0700

    Update update_container.sh

--- deployment/update_container.sh
+++ deployment/update_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker pull <dismallake>/<project5_img>:<v1.0>
-docker stop <project5_container>
-docker rm <project5_container>
-docker run -d -p <8000>:<8080> --name <project5_container> <dismallake>/<project5_img>:<v1.0>
+docker pull dismallake/project5_img:v1.0
+docker stop project5_container
+docker rm project5_container
+docker run -d -p 8000:8080 --name project5_container dismallake/project5_img:v1.0
 
